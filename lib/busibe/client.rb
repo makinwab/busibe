11aa533aeab9d882003f7dd5070fd32a74c4d417
andela-bmakinwa <makinwa37@gmail.com>
2016-04-14 02:52:35 +0100

    doc(client): use comments to document next steps

--- lib/busibe/client.rb
+++ lib/busibe/client.rb
@@ -16,6 +16,25 @@ module Busibe
       Configuration::VALID_CONFIG_KEYS.each do |key|
         send("#{key}=", merged_options[key])
       end
+
+      # create an exceptions/erros folder : contains error, client_error, server_error classes
+      # create a connection module : makes connection to faraday
+      # include Connection and Request class
+      # prepare_request : invokes the Connection class method
+      # create a request class / module : define_method :get/:post
+      # sendSMS return self
+      # checkAvailableCredits return self
+      # checkDeliveryStatus return self
+      #
+      # define a method_missing method that analyses methods with "_with_response"
+      # gets the method name by splitting string by ("_with_response")
+      # checks if method responds_to? class and calls method + gets response
+      # i.e sendSMS_with_response("How are you doing?")  
+      # http://technicalpickles.com/posts/using-method_missing-and-respond_to-to-create-dynamic-methods/
+      #
+      # getResponse return json_encoded response
     end
+
+    
   end # Client
 end
